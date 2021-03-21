d8ff487a17cc3c58380d402fe2f879571f74bddd
Bartholdi <79282610+Bartholdi@users.noreply.github.com>
2021-03-21 14:07:07 -0400

    test

---
+++ JupyterNotebooks/Lessons/lab1.java
@@ -0,0 +1,45 @@
+public class lab1{
+    public static void main(String[] args){
+//Joseph Bartholdi
+//Lab 1
+//3/01/21
+
+//Section 1
+//8
+//137
+//175
+//90
+
+//Section 2:
+//Boolean
+boolean is_cool = true;
+//byte
+byte h;
+//short
+short paper= 50000;
+//int
+int x = 4;
+//long
+long m= 12;
+//float
+float number= 1.0f;
+//double
+double bpm= 34.3;
+//char
+char char1= 'a';
+
+//Section 3
+//Make a List
+List<String> list = List.of("1", "2", "3", "4");
+
+//Section 4
+sum= (34 + 23);
+System.out.println(sum);
+
+//Section 5:
+float page = 15;
+System.out.println(page);
+page = 21;
+System.out.println(page);
+    }
+}
